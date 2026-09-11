619aeb312348e87a37c6b6d69c7660237d051544
eshitasp20 <eshitasp20@gmail.com>
2026-09-11 10:29:47 +0530

    Saving the file to gitHub

--- leapyear.py
+++ leapyear.py
@@ -1,15 +1,19 @@
-int year = 2026
-if (year % 4 ==0):
-    print("It is not a leap year")
+year = int (input("Enter a year:"))
+if (year % 4 == 0 ):
+    print("It is  a leap year")
 else:
     print("It is not a leap year")
 
 
-yr1=int(input("Enter 1 Year:\n")
-yr2=int(input("Enter other year:\n")
-        if (yr1 % 400 ==0) and (yr2 % 400 ==0):
-            print("Leap Year")
-        elif (yr1 % 100 ==0) and (yr2 % 100 ==0):
-             print(" Not Leap Year")
-        else:
-            print("Enter a valid input.")
+yr1 = int (input("Enter yr1:\n"))
+yr2 = int (input("Enter yr2:\n"))
+if( yr1 % 4 == 0 and (yr1 % 100 != 0 or yr1 % 400 == 0) ):
+     print(" yr1 is Leap Year")
+else:
+    print ("yr1 is not leap year")
+
+if( yr2 % 4 == 0 and (yr2 % 100 != 0 or yr2 % 400 == 0) ): 
+    print(" yr2 is Leap Year")
+else:
+     print(" yr2 is not Leap Year")
+
