b583524c98f3df584b4b0de58b116db40b122b4b
eshitasp20-sketch <eshitasp20@gmail.com>
2026-09-11 09:39:00 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -3,3 +3,13 @@ if (year % 4 ==0):
     print("It is not a leap year")
 else:
     print("It is not a leap year")
+
+
+yr1=int(input("Enter 1 Year:\n")
+yr2=int(input("Enter other year:\n")
+        if (yr1 % 400 ==0) and (yr2 % 400 ==0):
+            print("Leap Year")
+        elif (yr1 % 100 ==0) and (yr2 % 100 ==0):
+             print(" Not Leap Year")
+        else:
+            print("Enter a valid input.")
